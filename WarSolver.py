a9c9fadf5adcaabfb2f9c2e536dacc1e5919dad4
sebingel <sebingel+github@googlemail.com>
2016-09-23 12:54:05 +0200

    - reverted the order the stack works because the game apparently gives you the cards from top to bottom -.- - introduced the infamous InputGetter to handle input from  different sources (ie testcases) - we now can resolve war situations

--- WarSolver.py
+++ WarSolver.py
@@ -1,6 +1,3 @@
-import sys
-
-
 class Stack:
     def __init__(self):
         self.items = []
@@ -12,7 +9,7 @@ class Stack:
         self.items.append(item)
 
     def pop(self):
-        return self.items.pop()
+        return self.items.pop(0)
 
     def peek(self):
         return self.items[len(self.items) - 1]
@@ -20,8 +17,29 @@ class Stack:
     def size(self):
         return len(self.items)
 
-    def addatbottom(self, item):
-        self.items.insert(0, item)
+    def pushtobottom(self, item):
+        # self.items.insert(0, item)
+        self.push(item)
+
+
+class InputGetter:
+    def __init__(self):
+        self.__initial = True
+        self.__content = []
+
+    def getinput(self):
+        # return input()
+
+        if self.__initial:
+            self.__initial = False
+            with open("tc2.txt") as f:
+                self.__content = f.readlines()
+
+            for i in range(len(self.__content)):
+                if "\n" in self.__content[i]:
+                    self.__content[i] = self.__content[i][:-1]
+
+        return self.__content.pop(0)
 
 
 def getdigitvalue(value):
@@ -49,37 +67,73 @@ def comparecards(card1, card2):
         return 0
 
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
+inp = InputGetter()
 
 cardsPlayerOne = Stack()
 cardsPlayerTwo = Stack()
 
-n = int(input())  # the number of cards for player 1
+n = int(inp.getinput())  # the number of cards for player 1
 for i in range(n):
-    cardsPlayerOne.push(input())  # the n cards of player 1
-m = int(input())  # the number of cards for player 2
+    cardsPlayerOne.push(inp.getinput())  # the n cards of player 1
+m = int(inp.getinput())  # the number of cards for player 2
 for j in range(m):
-    cardsPlayerTwo.push(input())  # the m cards of player 2
+    cardsPlayerTwo.push(inp.getinput())  # the m cards of player 2
 
 # Write an action using print
 # To debug: print("Debug messages...", file=sys.stderr)
 
-count = 0
+turnCount = 0
+pat = False
 
 while cardsPlayerOne.size() > 0 and cardsPlayerTwo.size() > 0:
-    count += 1
+    turnCount += 1
+
     cardPlayerOne = cardsPlayerOne.pop()
     cardPlayerTwo = cardsPlayerTwo.pop()
 
+    cardsAtStakePlayerOne = [cardPlayerOne]
+    cardsAtStakePlayerTwo = [cardPlayerTwo]
+
     result = comparecards(cardPlayerOne, cardPlayerTwo)
 
-    if result == -1:  # player 1 won
-        cardsPlayerOne.addatbottom(cardPlayerTwo)
-    elif result == 1:  # player 2 won
-        cardsPlayerTwo.addatbottom(cardPlayerOne)
+    if result == 0:
+        turnCount += 1
 
-if cardsPlayerOne.size() < 1:
-    print("2 " + str(count))
-if cardsPlayerTwo.size() < 1:
-    print("1 " + str(count))
+    while result == 0:
+        # here starts the war
+        if cardsPlayerOne.size() < 3 or cardsPlayerTwo.size() < 3:
+            print("PAT")
+            pat = True
+            break
+
+        cardsAtStakePlayerOne.append(cardsPlayerOne.pop())
+        cardsAtStakePlayerOne.append(cardsPlayerOne.pop())
+        cardsAtStakePlayerOne.append(cardsPlayerOne.pop())
+        cardsAtStakePlayerTwo.append(cardsPlayerTwo.pop())
+        cardsAtStakePlayerTwo.append(cardsPlayerTwo.pop())
+        cardsAtStakePlayerTwo.append(cardsPlayerTwo.pop())
+
+        cardPlayerOne = cardsPlayerOne.pop()
+        cardPlayerTwo = cardsPlayerTwo.pop()
+
+        cardsAtStakePlayerOne.append(cardPlayerOne)
+        cardsAtStakePlayerTwo.append(cardPlayerTwo)
+
+        result = comparecards(cardPlayerOne, cardPlayerTwo)
+
+    if result == -1:  # player 1 won
+        for card in cardsAtStakePlayerOne:
+            cardsPlayerOne.pushtobottom(card)
+        for card in cardsAtStakePlayerTwo:
+            cardsPlayerOne.pushtobottom(card)
+    elif result == 1:  # player 2 won
+        for card in cardsAtStakePlayerOne:
+            cardsPlayerTwo.pushtobottom(card)
+        for card in cardsAtStakePlayerTwo:
+            cardsPlayerTwo.pushtobottom(card)
+
+if not pat:
+    if cardsPlayerOne.size() < 1:
+        print("2 " + str(turnCount))
+    if cardsPlayerTwo.size() < 1:
+        print("1 " + str(turnCount))
